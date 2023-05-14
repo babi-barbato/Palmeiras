fa4c85d4a98c4803ca3b9e860812f0ae04b76535
babi-barbato <barbara.oliveira@sptech.school>
2023-05-14 01:42:21 -0300

    Pag Index, slideshow e caixas

---
+++ index/index.js
@@ -0,0 +1,46 @@
+function btn1(){
+    //Mudando cor dos botões
+    button1.style.backgroundColor = "#1D4F2B";
+    button2.style.backgroundColor = "#6ec287";
+    button3.style.backgroundColor = "#6ec287";
+    button4.style.backgroundColor = "#6ec287";
+
+    //mudando imagens
+    ft1.style.display = "block";
+    ft2.style.display = "none";
+    ft3.style.display = "none";
+    ft4.style.display = "none";
+}
+function btn2(){
+    button2.style.backgroundColor = "#1D4F2B";
+    button1.style.backgroundColor = "#6ec287";
+    button3.style.backgroundColor = "#6ec287";
+    button4.style.backgroundColor = "#6ec287";
+
+    ft2.style.display = "block";
+    ft1.style.display = "none";
+    ft3.style.display = "none";
+    ft4.style.display = "none";
+}
+function btn3(){
+    button3.style.backgroundColor = "#1D4F2B";
+    button1.style.backgroundColor = "#6ec287";
+    button2.style.backgroundColor = "#6ec287";
+    button4.style.backgroundColor = "#6ec287";
+
+    ft3 .style.display = "block";
+    ft1.style.display = "none";
+    ft2.style.display = "none";
+    ft4.style.display = "none";
+}
+function btn4(){
+    button4.style.backgroundColor = "#1D4F2B";
+    button1.style.backgroundColor = "#6ec287";
+    button2.style.backgroundColor = "#6ec287";
+    button3.style.backgroundColor = "#6ec287";
+    
+    ft4 .style.display = "block";
+    ft1.style.display = "none";
+    ft2.style.display = "none";
+    ft3.style.display = "none";
+}
